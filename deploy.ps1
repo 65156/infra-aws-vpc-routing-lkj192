007c50ddcd0adb53a149e48175471a28c884c297
Fraser Smith <smithfra@192-168-1-9.tpgi.com.au>
2020-02-11 13:04:07 +1100

    added additional functionality with rollback setting

--- deploy.ps1
+++ deploy.ps1
@@ -9,11 +9,31 @@ foreach($a in $accounts){
     $connections = $a.connections
     
     #Switch to applicable Account
-    Switch-RoleAlias $account admin 
+    
     Write-Host "Processing $account" -f white -b magenta 
+    Switch-RoleAlias $account admin 
+    Write-Host ""
     $routeTables = (Get-EC2RouteTable -region $region)
     
-    
+    $rollbackstatus = $false
+    if($rollbackstatus -eq $true){
+        $lines = Import-CSV $rollbackfile
+        Write-Host "Rolling back changes." -f yellow
+        foreach($l in $lines){
+
+        $acc = $l.Account
+        $Rt = $l.RouteTable
+        $r = $l.route
+        $c = $l.connection
+        if($acc -eq $account){
+            #do stuff
+            #configure command to reconfigure route to original here.
+            Write-Host "$acc, $rt, $r, $c" -f magenta
+            }
+        }
+    continue
+    }
+
     foreach($rT in $routeTables){
         $routes = $rT.Routes
         $routeTable = $rT.RouteTableID
@@ -44,7 +64,7 @@ foreach($a in $accounts){
                     #Write-Host "Updating Route for $route" -f green ; 
                     #Set-EC2Route -DestinationCidrBlock $cidr -RouteTableId $routeTable -TransitGatewayId $transitgatewayID -Region $Region 
                     } catch { 
-                        #Write-Host "Route set failure for $route" -f red 
+                        Write-Host "Route set failure for $route" -f red 
                         } 
                 }
             if($match -eq 0){Write-Host "No Match in $route"}
@@ -53,12 +73,14 @@ foreach($a in $accounts){
             } 
             Write-Host ""
         }
-        Write-Host ""
-    }
 
-    #Create Rollback CSV file from Hash Table
-    Write-Host "Rollback Data" -f white -b magenta
-    $rollback 
-    Write-Host "Exporting to CSV." -f yellow
+    }
+    # terminate script early if rolling back otherwise exports file will be overwritten with null values.
+    if($rollbackstatus -eq $true){exit} 
+
     $rollback | Export-CSV $rollbackfile -force
+    #Create Rollback CSV file from Hash Table
+    Write-Host "Exporting Rollback Data to CSV" -f white -b magenta
+    $rollback 
+
     
--- variables.ps1
+++ variables.ps1
@@ -1,11 +1,11 @@
-$rollbackfile = ./files/rollbackdata.csv
 $region = "ap-southeast-2"
 $transitgatewayID = "tgw-0f5a6a47f6a25f9f9"
+$rollbackstatus = $false
 $accounts = @( 
     # accounts are dependant on account names configured in PS-AWS-SSO-AUTH.psm1
     #[PSCustomObject]@{Account="PipelineProd"; connections=@("vgw-e6ccfefb","pcx-0518b26c","pcx-08423361","pcx-689e3701","pcx-a0b325c9","pcx-aa6cd9c3","pcx-d56cd9bc")},
     #[PSCustomObject]@{Account="PipelineDev"; connections=@("vgw-a52615b8","pcx-08423361","pcx-c47ebdad","pcx-d96cd9b0")},
     #[PSCustomObject]@{Account="LegacyProd"; connections=@("vgw-e72615fa","vgw-e62615fb","pcx-a86cd9c1","pcx-aa6cd9c3","pcx-c47ebdad","pcx-cf70bda6","pcx-d26cd9bb","pcx-d56cd9bc","pcx-689e3701","pcx-689e3701")}
-    #[PSCustomObject]@{Account="LegacyDev"; connections=@("vgw-a52615b8","pcx-08423361","pcx-c47ebdad","pcx-d96cd9b0")},
+    [PSCustomObject]@{Account="LegacyDev"; connections=@("vgw-a52615b8","pcx-08423361","pcx-c47ebdad","pcx-d96cd9b0")},
     [PSCustomObject]@{Account="SandboxD3"; connections=@("pcx-0aeae2dd703bc4755")}
     ) 
